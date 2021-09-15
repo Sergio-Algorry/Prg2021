1e8fb36139dca1c2aab1dcad6583ae18cfe84764
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2021-09-15 10:52:59 -0300

    clase de hoy

---
+++ PruebaTry/PruebaTry/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace PruebaTry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btIgual = new System.Windows.Forms.Button();
+            this.txtNumerador = new System.Windows.Forms.TextBox();
+            this.txtDenominador = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btIgual
+            // 
+            this.btIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btIgual.Location = new System.Drawing.Point(421, 45);
+            this.btIgual.Name = "btIgual";
+            this.btIgual.Size = new System.Drawing.Size(75, 35);
+            this.btIgual.TabIndex = 0;
+            this.btIgual.Text = "=";
+            this.btIgual.UseVisualStyleBackColor = true;
+            this.btIgual.Click += new System.EventHandler(this.btIgual_Click);
+            // 
+            // txtNumerador
+            // 
+            this.txtNumerador.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumerador.Location = new System.Drawing.Point(62, 45);
+            this.txtNumerador.Name = "txtNumerador";
+            this.txtNumerador.Size = new System.Drawing.Size(100, 32);
+            this.txtNumerador.TabIndex = 1;
+            this.txtNumerador.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtNumerador_KeyUp);
+            // 
+            // txtDenominador
+            // 
+            this.txtDenominador.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDenominador.Location = new System.Drawing.Point(243, 45);
+            this.txtDenominador.Name = "txtDenominador";
+            this.txtDenominador.Size = new System.Drawing.Size(100, 32);
+            this.txtDenominador.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(560, 54);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 26);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.Location = new System.Drawing.Point(62, 143);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 26);
+            this.lblMensaje.TabIndex = 4;
+            this.lblMensaje.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(188, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "/";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 195);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtDenominador);
+            this.Controls.Add(this.txtNumerador);
+            this.Controls.Add(this.btIgual);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btIgual;
+        private System.Windows.Forms.TextBox txtNumerador;
+        private System.Windows.Forms.TextBox txtDenominador;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PruebaTry/PruebaTry/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaTry
+{
+    public partial class Form1 : Form
+    {
+        int contador = 0;
+        DateTime PEPE = System.Convert.ToDateTime("08/05/2021 10:45");
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void btIgual_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblResultado.Text = (Convert.ToInt32(txtNumerador.Text)
+                    / Convert.ToInt32(txtDenominador.Text)).ToString();
+
+                if(lblResultado.Text !="6")
+                {
+                    Exception error = new Exception("No es el resultado que esperaba");
+                    throw error;
+                }
+            }
+            catch (DivideByZeroException pepe)
+            {
+                lblMensaje.Text = pepe.Message;
+                txtDenominador.Text = "";
+                lblMensaje.Visible = true;
+            }
+            catch (Exception pepe)
+            {
+                lblMensaje.Text = pepe.Message;
+                lblMensaje.Visible = true;
+            }
+        }
+
+        private void txtNumerador_KeyUp(object sender, KeyEventArgs e)
+        {
+           if( e.KeyCode == Keys.A)
+            {
+                lblMensaje.Text = "pepe";
+                lblMensaje.Visible = true;
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PEPE = PEPE.AddSeconds(1);
+            lblResultado.Text = PEPE.ToString();
+
+        }
+    }
+}
