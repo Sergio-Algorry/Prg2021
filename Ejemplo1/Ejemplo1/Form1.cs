f793749cf23cc51be9bfe66b92cee13c5d522308
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2021-09-22 10:07:59 -0300

    Prueba

---
+++ Ejemplo1/Ejemplo1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox3.Focus();
+            this.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+
+            textBox3.Enabled = false;
+
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            textBox1.Visible = false;
+            textBox1.Text = "";
+            textBox2.Visible = false;
+            textBox2.Text = "";
+
+            textBox3.Enabled = true;
+            textBox3.Focus();
+
+            textBox3.SelectAll();
+
+        }
+    }
+}
