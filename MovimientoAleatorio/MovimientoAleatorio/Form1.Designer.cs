25a8cce6595794ba8936bbfa4d7fed59375cf90e
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2021-09-22 10:54:27 -0300

    finalizado

---
+++ MovimientoAleatorio/MovimientoAleatorio/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace MovimientoAleatorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.bt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.BackColor = System.Drawing.Color.Red;
+            this.lbl.Location = new System.Drawing.Point(565, 293);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(100, 68);
+            this.lbl.TabIndex = 0;
+            // 
+            // bt
+            // 
+            this.bt.Location = new System.Drawing.Point(725, 0);
+            this.bt.Name = "bt";
+            this.bt.Size = new System.Drawing.Size(75, 23);
+            this.bt.TabIndex = 1;
+            this.bt.Text = "Inicio";
+            this.bt.UseVisualStyleBackColor = true;
+            this.bt.Click += new System.EventHandler(this.bt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1357, 639);
+            this.Controls.Add(this.bt);
+            this.Controls.Add(this.lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button bt;
+    }
+}
+
---
+++ MovimientoAleatorio/MovimientoAleatorio/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovimientoAleatorio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Click(object sender, EventArgs e)
+        {
+            int sentidoV = 1;
+            int sentidoH = 1;
+            int pasoV = 1;
+            int pasoH = 1;
+
+            Random randoPaso = new Random();
+            Random randoSentido = new Random();
+            int j = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i > 10 * j)
+                {
+                    sentidoH = randoSentido.Next(-1, 2);
+                    sentidoV = randoSentido.Next(-1, 2);
+                    j = j + 1;
+                }
+                pasoH = randoPaso.Next(0, 3);
+                pasoV = randoPaso.Next(0, 3);
+
+                lbl.Left = lbl.Left + (pasoH * sentidoH);
+                lbl.Top = lbl.Top + (pasoV * sentidoV);
+                this.Refresh();
+            }
+        }
+    }
+}
