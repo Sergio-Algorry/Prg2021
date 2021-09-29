adb54703373278aa2e1db4da7970c2732c652ae5
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2021-09-29 10:12:04 -0300

    primera version

---
+++ MiPrimerArreglo/MiPrimerArreglo/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace MiPrimerArreglo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblLista = new System.Windows.Forms.Label();
+            this.btMostrar = new System.Windows.Forms.Button();
+            this.txtCant = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btDimensionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DIAS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 1;
+            // 
+            // btCargar
+            // 
+            this.btCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCargar.Location = new System.Drawing.Point(397, 65);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(75, 26);
+            this.btCargar.TabIndex = 2;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.btCargar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(234, 69);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(157, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // lblLista
+            // 
+            this.lblLista.AutoSize = true;
+            this.lblLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLista.Location = new System.Drawing.Point(228, 103);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(0, 31);
+            this.lblLista.TabIndex = 4;
+            // 
+            // btMostrar
+            // 
+            this.btMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMostrar.Location = new System.Drawing.Point(479, 65);
+            this.btMostrar.Name = "btMostrar";
+            this.btMostrar.Size = new System.Drawing.Size(75, 26);
+            this.btMostrar.TabIndex = 5;
+            this.btMostrar.Text = "Mostrar";
+            this.btMostrar.UseVisualStyleBackColor = true;
+            this.btMostrar.Click += new System.EventHandler(this.btMostrar_Click);
+            // 
+            // txtCant
+            // 
+            this.txtCant.Location = new System.Drawing.Point(234, 20);
+            this.txtCant.Name = "txtCant";
+            this.txtCant.Size = new System.Drawing.Size(100, 20);
+            this.txtCant.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cantidad de nombres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nombre";
+            // 
+            // btDimensionar
+            // 
+            this.btDimensionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDimensionar.Location = new System.Drawing.Point(386, 10);
+            this.btDimensionar.Name = "btDimensionar";
+            this.btDimensionar.Size = new System.Drawing.Size(108, 39);
+            this.btDimensionar.TabIndex = 9;
+            this.btDimensionar.Text = "Dimensionar";
+            this.btDimensionar.UseVisualStyleBackColor = true;
+            this.btDimensionar.Click += new System.EventHandler(this.btDimensionar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 450);
+            this.Controls.Add(this.btDimensionar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCant);
+            this.Controls.Add(this.btMostrar);
+            this.Controls.Add(this.lblLista);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btCargar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblLista;
+        private System.Windows.Forms.Button btMostrar;
+        private System.Windows.Forms.TextBox txtCant;
+        protected System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btDimensionar;
+    }
+}
+
---
+++ MiPrimerArreglo/MiPrimerArreglo/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerArreglo
+{
+    public partial class Form1 : Form
+    {
+        //string[] nombres = new string[1000];
+        string[] nombres;
+        int pos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btDimensionar_Click(object sender, EventArgs e)
+        {
+            int cant = Convert.ToInt32(txtCant.Text);
+            nombres = new string[cant];
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+            nombres[pos]= txtNombre.Text;
+            pos = pos + 1;
+
+            txtNombre.Focus();
+            txtNombre.SelectAll();
+        }
+
+        private void btMostrar_Click(object sender, EventArgs e)
+        {
+            lblLista.Text = "";
+            foreach (string item in nombres)
+            {
+                lblLista.Text = lblLista.Text + item + "\r\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ////Intancia
+            //string[] Dias;
+            ////inicializando
+            //Dias = new string[7];
+            ////dando valor a cada posicion del arreglo
+            //Dias[0] = "Lunes";
+            //Dias[1] = "Martes";
+            //Dias[2] = "Miercoles";
+            //Dias[3] = "Jueves";
+            //Dias[4] = "Viernes";
+            //Dias[5] = "Sábado";
+            //Dias[6] = "Domingo";
+
+            //Intancia - inicializando - dando valor a cada posicion del arreglo
+            string[] Dias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+
+            //for (int i = 0; i < Dias.Length; i++)
+            //{
+            //    label1.Text = label1.Text + Dias[i] + "\r\n";
+            //}
+
+            foreach (string item in Dias)
+            {
+                label1.Text = label1.Text + item + "\r\n";
+            }
+        }
+    }
+}
