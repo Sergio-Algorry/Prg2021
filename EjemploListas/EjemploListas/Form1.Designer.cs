1567869fff5bb218e42160042f9c93f0b84a418b
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2021-10-05 11:34:36 -0300

    Primera Versión

---
+++ EjemploListas/EjemploListas/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace EjemploListas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.lblLista = new System.Windows.Forms.Label();
+            this.btMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(27, 44);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(201, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btCargar
+            // 
+            this.btCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCargar.Location = new System.Drawing.Point(578, 38);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(88, 30);
+            this.btCargar.TabIndex = 2;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.btCargar_Click);
+            // 
+            // lblLista
+            // 
+            this.lblLista.AutoSize = true;
+            this.lblLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLista.Location = new System.Drawing.Point(27, 71);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(72, 31);
+            this.lblLista.TabIndex = 3;
+            this.lblLista.Text = "Lista";
+            // 
+            // btMostrar
+            // 
+            this.btMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMostrar.Location = new System.Drawing.Point(685, 38);
+            this.btMostrar.Name = "btMostrar";
+            this.btMostrar.Size = new System.Drawing.Size(88, 30);
+            this.btMostrar.TabIndex = 4;
+            this.btMostrar.Text = "Mostrar";
+            this.btMostrar.UseVisualStyleBackColor = true;
+            this.btMostrar.Click += new System.EventHandler(this.btMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btMostrar);
+            this.Controls.Add(this.lblLista);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btCargar;
+        private System.Windows.Forms.Label lblLista;
+        private System.Windows.Forms.Button btMostrar;
+    }
+}
+
---
+++ EjemploListas/EjemploListas/Form1.cs
@@ -0,0 +1,40 @@
+﻿using EjemploListas.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploListas
+{
+    public partial class Form1 : Form
+    {
+        public Persona[] Personas { get; set; } = new Persona[3];
+        public int Pos = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = txtNombre.Text;
+            Pos = Pos + 1;
+            Personas[Pos] = persona;
+        }
+
+        private void btMostrar_Click(object sender, EventArgs e)
+        {
+            lblLista.Text = "Lista:\r\n";
+            foreach (Persona item in Personas)
+            {
+                lblLista.Text = lblLista.Text + item.Nombre + "\r\n";
+            }
+        }
+    }
+}
